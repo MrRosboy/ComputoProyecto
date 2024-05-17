7b93b6ccc02074f46a8ca52fe09b454428a9aac9
MrRosboy <166888706+MrRosboy@users.noreply.github.com>
2024-05-17 14:55:22 -0600

    Create conexion.php
    create conection to db

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "servidorproyecto.mysql.database.azure.com";
+$username = "adminProyecto";
+$password = "Edgar270901##$";
+$dbname = "dbproyecto2";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
