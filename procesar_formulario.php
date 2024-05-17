5ecaaffbbcd514c7934bd8b7e1947ce9cce9bf20
MrRosboy <166888706+MrRosboy@users.noreply.github.com>
2024-05-17 14:58:30 -0600

    Create procesar_formulario.php

---
+++ procesar_formulario.php
@@ -0,0 +1,30 @@
+<!-- procesar_formulario.php -->
+<?php
+$servername = "servidorproyecto.mysql.database.azure.com";
+$username = "adminProyecto";
+$password = "Edgar270901##$";
+$dbname = "dbproyecto2";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Obtener datos del formulario
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+
+// Insertar datos en la base de datos
+$sql = "INSERT INTO usuarios (nombre, email) VALUES ('$nombre', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Nuevo registro creado exitosamente";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
