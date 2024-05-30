5cc660d5fde0b315f032b41af9f307504fbe7fac
MrRosboy <166888706+MrRosboy@users.noreply.github.com>
2024-05-30 17:31:19 -0600

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Datos de autenticación
+    $correct_username = 'admin';
+    $correct_password = 'password';
+
+    if ($username == $correct_username && $password == $correct_password) {
+        $_SESSION['loggedin'] = true;
+        header('Location: formulario.php');
+        exit;
+    } else {
+        $error_message = 'Usuario o contraseña incorrectos';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="login.php" method="POST">
+        <label for="username">Usuario:</label><br>
+        <input type="text" id="username" name="username" required><br>
+        <label for="password">Contraseña:</label><br>
+        <input type="password" id="password" name="password" required><br>
+        <input type="submit" value="Iniciar sesión"><br><br>
+    </form>
+    <?php
+    if (isset($error_message)) {
+        echo '<p style="color:red;">' . $error_message . '</p>';
+    }
+    ?>
+</body>
+</html>
+
