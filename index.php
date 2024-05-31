641cfd2bf439e694163d4658b673436a52f0a9dc
MrRosboy <166888706+MrRosboy@users.noreply.github.com>
2024-05-30 23:20:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($username === $correct_username && $password === $correct_password) {
         $_SESSION['loggedin'] = true;
-        header('Location: formulario.php');
+        header('Location: index.php');
         exit;
     } else {
         $error_message = 'Usuario o contraseña incorrectos';
